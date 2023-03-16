e06c24d57ada19e35cdf2d2bbd5c1b65d209c229
Samuel Eromosele <erosam.se@gmail.com>
2023-03-16 20:47:10 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * puts prints string to stdio with a trailing new line
+ */
+int main(void)
+{
+	puts(char "Programming is like building a multilingual puzzle");
+
+	return (0);
+}
