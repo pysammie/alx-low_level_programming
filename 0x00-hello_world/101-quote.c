200a0bbae4ff32e92ddd622135024373be5b5eec
Samuel Eromosele <erosam.se@gmail.com>
2023-03-17 01:37:41 +0000

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd>
+
+/**
+ * main - Entry point
+ *
+ * write prints with a file descriptor of 1
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
