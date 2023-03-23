bdfeeaf6c328a619a4da87afea4414a1d6d2d6ea
Samuel Eromosele <erosam.se@gmail.com>
2023-03-23 21:41:50 +0000

    Printing lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: Integer value of how many lines
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
