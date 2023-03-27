8a9a948dd41810fe4ccfdd79b27312e8b59a1279
Samuel Eromosele <erosam.se@gmail.com>
2023-03-27 22:33:18 +0000

    Half of a second string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,13 +15,13 @@ void puts_half(char *str)
 		len++;
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; i <= len; i++)
+		for (i = len / 2; i < len; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
 		n = (len - 1) / 2;
-		for (i = (n + 1); i <= len; i++)
+		for (i = (n + 1); i < len; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
