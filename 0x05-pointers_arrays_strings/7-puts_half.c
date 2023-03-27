2b07b4737cc1381633f006058efebe1b99c0da7c
Samuel Eromosele <erosam.se@gmail.com>
2023-03-27 22:28:49 +0000

    Half of a second string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: String
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int len;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i <= len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		n = (len - 1) / 2;
+		for (i = (n + 1); i <= len; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
