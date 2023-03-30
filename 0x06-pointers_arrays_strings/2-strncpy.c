86ac5c87f59408c41ccfa59e61d15465c8db4ae0
Samuel Eromosele <erosam.se@gmail.com>
2023-03-30 10:21:43 +0000

    Copying strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -43,12 +43,9 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	else
 	{
-		if (n <= len_dest)
+		for (i = 0; i < n; i++)
 		{
-			for (i = 0; i < n; i++)
-			{
-				dest[i] = src[i];
-			}
+			dest[i] = src[i];
 		}
 	}
 	return (dest);
