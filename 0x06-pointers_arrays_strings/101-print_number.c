11b2c36679b5793887fda829a5c85e6ecc7a03fa
Samuel Eromosele <erosam.se@gmail.com>
2023-03-31 18:55:24 +0000

    Print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer
+ *
+ * @n: integer
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
+}
