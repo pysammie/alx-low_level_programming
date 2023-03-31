f9dce7aeccc7caec8ffb678cc891d161e70b60f3
Samuel Eromosele <erosam.se@gmail.com>
2023-03-31 09:06:46 +0000

    Capitalizing a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitilizes every word in a string
+ *
+ * @str: string
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (str[i - 1] == 10 ||
+				str[i - 1] == 32 ||
+				str[i - 1] == 33 ||
+				str[i - 1] == 34 ||
+				str[i - 1] == 40 ||
+				str[i - 1] == 41 ||
+				str[i - 1] == 44 ||
+				str[i - 1] == 46 ||
+				str[i - 1] == 59 ||
+				str[i - 1] == 63 ||
+				str[i - 1] == 123 ||
+				str[i - 1] == 125 ||
+				str[i - 1] == 9)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
