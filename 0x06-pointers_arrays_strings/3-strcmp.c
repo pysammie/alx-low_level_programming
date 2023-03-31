19e53f74271e2781b7fc8e4daee28c428cc32af9
Samuel Eromosele <erosam.se@gmail.com>
2023-03-31 10:23:51 +0000

    Comparing strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,63 +11,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int len_s1;
-	int len_s2;
-	int comp;
 
-	len_s1 = 0;
-	len_s2 = 0;
-	comp = 0;
-	for (i = 0; s1[i] != '\0'; i++)
-		len_s1++;
-	for (i = 0; s2[i] != '\0'; i++)
-		len_s2++;
-	if (len_s1 <= len_s2)
+	for (i = 0; (s1[i] != '\0' || s2[i] != '\0'); i++)
 	{
-		for (i = 0; i <= len_s1; i++)
-		{
-			comp = s1[i] - s2[i];
-			if (comp == 0)
-			{
-				if (i != len_s1)
-					continue;
-				else
-				{
-					if (len_s1 != len_s2)
-					{
-						comp = 0 - s2[i + 1];
-					}
-					else
-					{
-						break;
-					}
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		else
+			continue;
 	}
-	else
-	{
-		for (i = 0; i <= len_s2; i++)
-		{
-			comp = s1[i] - s2[i];
-			if (comp == 0)
-			{
-				if (i != len_s2)
-					continue;
-				else
-				{
-					comp = s1[i + 1] - 0;
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
-	}
-	return (comp);
+	return (0);
 }
