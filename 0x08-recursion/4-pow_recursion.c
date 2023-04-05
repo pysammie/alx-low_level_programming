eff9090801840152041c43c95bc926bbbcfe82f1
Samuel Eromosele <erosam.se@gmail.com>
2023-04-05 19:16:55 +0000

    Printing factorials and powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns value x ^ y
+ *
+ * @x: int
+ * @y: int
+ *
+ * Return: -1 (Error) or int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
