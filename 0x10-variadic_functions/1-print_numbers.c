59dbc2b4918b7fbe30fa0d956199c02e838982eb
Samuel Eromosele <erosam.se@gmail.com>
2023-04-20 19:39:21 +0000

    Prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers
+ *
+ * @separator: string seperator
+ * @n: no of integers passed to function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list args;
+
+	if (n <= 0)
+		exit(1);
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
