a6bd6cd4d5ea8825903d5cada76bf2332e370e76
Samuel Eromosele <erosam.se@gmail.com>
2023-04-20 18:41:05 +0000

    Printing sum of all parameters

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
