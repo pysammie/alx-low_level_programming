60acd2d29130de6cc353f2578835991fe342d446
Samuel Eromosele <erosam.se@gmail.com>
2023-04-27 23:04:32 +0000

    Frees linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees an entire linked list
+ *
+ * @head: pointer to linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while(head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
