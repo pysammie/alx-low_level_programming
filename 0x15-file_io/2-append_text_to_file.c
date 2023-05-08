88d314646c965d2ea33e63bac40eab345f20811c
Samuel Eromosele <erosam.se@gmail.com>
2023-05-08 23:44:12 +0000

    Appends text to end of file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to end of file
+ *
+ * @filename: name of file
+ * @text_content: content to append
+ *
+ * Return: 1(Success) or -1(Failure)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
