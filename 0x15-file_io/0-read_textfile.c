a076b25f5f105ba6f1d1ea6a576f6330e8ef7b82
Samuel Eromosele <erosam.se@gmail.com>
2023-05-09 06:56:30 +0000

    Fix errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n;
+	ssize_t n, w;
 	char buffer[5000];
 
 	if (filename == NULL)
@@ -22,18 +22,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
+		close(fd);
 		return (0);
 
 	if (fd != -1)
 	{
 		n = read(fd, buffer, letters);
 		if (n > 0)
-			write(STDOUT_FILENO, buffer, n);
+		{
+			w = write(STDOUT_FILENO, buffer, n);
+			if (w == -1 || w != n)
+			{
+				close(fd);
+				return (0);
+			}
+		}
 		else
+			close(fd);
 			return (0);
 	}
 
 	close(fd);
 
-	return (n);
+	return (w);
 }
