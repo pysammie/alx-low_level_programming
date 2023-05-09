6056b11be9b9056d2027dff336a66396057b259a
Samuel Eromosele <erosam.se@gmail.com>
2023-05-09 11:46:41 +0000

    Append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,29 +6,28 @@
  * @filename: name of file
  * @text_content: content to append
  *
- * Return: 1(Success) -1(Failure)
+ * Return: 1(Success) or -1(Failure)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		w = write(fd, text_content, strlen(text_content));
-
-		if (w == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(fd);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	return (-1);
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
