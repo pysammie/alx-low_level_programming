3bdf6ccb6fb94b1c5b8a3d6e0474d84c1be73c4b
Samuel Eromosele <erosam.se@gmail.com>
2023-05-09 10:04:05 +0000

    Fix Errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,22 +10,32 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd_toappend, fd_toread;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd_toappend = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == -1)
+	if (fd_toappend == -1)
 		return (-1);
 
-	if (write(fd, text_content, strlen(text_content)) == -1)
+	if (write(fd_toappend, text_content, strlen(text_content)) == -1)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		fd_toread = open(filename, O_RDONLY);
 
-	close(fd);
+		if (fd_toread == -1)
+			return (-1);
+
+		close(fd_toread);
+		return (1);
+	}
+
+	close(fd_toappend);
 
 	return (1);
 }
