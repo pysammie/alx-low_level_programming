97d4fd2e1c24ca1d4362921dc7e6a54b559b184d
Samuel Eromosele <erosam.se@gmail.com>
2023-05-09 11:10:27 +0000

    Fix errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
+		for (len = 0; text_content[len];)
 		{
 			len++;
 		}
