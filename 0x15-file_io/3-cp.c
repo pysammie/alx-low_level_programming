4a43cc545d0090f6e235f81d2ec2d28924fd004f
Samuel Eromosele <erosam.se@gmail.com>
2023-05-10 10:18:34 +0000

    Copy files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,12 +42,14 @@ int main(int ac, char **av)
 		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99); }
 	}
-	file_close = close(file_from);
-	if (file_close == -1)
+	if (char_rdwr == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[0]);
+		exit(98); }
+	if (close(file_from) == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100); }
-	file_close = close(file_to);
-	if (file_close == -1)
+
+	if (close(file_to) == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100); }
 	return (0);
