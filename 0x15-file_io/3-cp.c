c3a4d454356f05db15ef42eb03b6a6a38ce38d65
Samuel Eromosele <erosam.se@gmail.com>
2023-05-10 09:52:17 +0000

    Copy files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void error_handle(int, int, char **);
 /**
  * main - program copies one file to another
  *
@@ -21,56 +20,38 @@ int main(int ac, char **av)
 	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
+	{	dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97); }
 	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+		exit(98); }
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	error_handle(file_from, file_to, av);
+	if (file_to == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99); }
 
 	char_rdwr = 1024;
 	while ((char_rdwr = read(file_from, buffer, 1024)) > 0)
 	{
 		if (char_rdwr == -1)
-			error_handle(-1, 0, av);
-
+		{	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+			exit(98); }
 		char_rdwr = write(file_to, buffer, char_rdwr);
 
 		if (char_rdwr == -1)
-			error_handle(0, -1, av);
+		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99); }
 	}
-
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	if (close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
-}
-/**
- * error_handle - handles error in reading from file or writing to file
- *
- * @file_from: fd of file to be read from
- * @file_to: fd of file to be written to
- * @av: array of filenames
- */
-void error_handle(int file_from, int file_to, char **av)
-{
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
-		exit(99);
-	}
 }
