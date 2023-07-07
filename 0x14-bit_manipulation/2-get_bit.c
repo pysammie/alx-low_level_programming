ba301db1c6b6cdbbbd580f9839c3f64317923cb5
Samuel Eromosele <erosam.se@gmail.com>
2023-07-07 19:36:02 +0000

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - gets bit at a given index
+ *
+ * @n: number
+ * @index: Index
+ *
+ * Return: bit at index or -1 (Error)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_value;
+
+	if (index > 63)
+		return (-1);
+
+	bit_value = (n >> index) & 1;
+
+	return (bit_value);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets bit at given position to 0
+ *
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 (Success) -1 (Failure)
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index == 63)
+		return (-1);
+
+	*n = *n & ~(1 << index);
+
+	return (1);
+}
