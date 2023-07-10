5e6c001ac4e1bc26ce0dd9da2fd7f69ceaf15414
Samuel Eromosele <erosam.se@gmail.com>
2023-07-10 19:19:00 +0000

    File i/o with system calls

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a textfile and prints it to
+ * POSIX standard output
+ *
+ * @filename: name of file
+ * @letters: no of letters it should read/print
+ *
+ * Return: no of letters read/printed or 0 (Failure)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n, w;
+	char buffer[5000];
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	if (fd != -1)
+	{
+		n = read(fd, buffer, letters);
+		if (n > 0)
+		{
+			w = write(STDOUT_FILENO, buffer, n);
+			if (w == -1 || w != n)
+			{
+				close(fd);
+				return (0);
+			}
+		}
+		else
+		{
+			close(fd);
+			return (0);
+		}
+	}
+
+	close(fd);
+
+	return (w);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * main - program copies one file to another
+ *
+ * @ac: argument counter
+ * @av: argument vector
+ *
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int file_from, file_to, char_rdwr;
+	char buffer[1024];
+
+	if (ac != 3)
+	{	dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97); }
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+		exit(98); }
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99); }
+
+	while ((char_rdwr = read(file_from, buffer, 1024)) > 0)
+	{
+		if (char_rdwr == -1)
+		{	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+			exit(98); }
+		char_rdwr = write(file_to, buffer, char_rdwr);
+
+		if (char_rdwr == -1)
+		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99); }
+	}
+	if (char_rdwr == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+		exit(98); }
+	if (close(file_from) == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100); }
+
+	if (close(file_to) == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100); }
+
+	return (0);
+}
